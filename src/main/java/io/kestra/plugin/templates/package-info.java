79815954cb4f880c9948a56323c94f6719cd93f3
thyw1 <62542811+thyw1@users.noreply.github.com>
2025-08-28 16:45:09 +0800

    Initial commit

---
+++ src/main/java/io/kestra/plugin/templates/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Example plugin",
+    description = "A plugin to show how to build a plugin in Kestra.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.templates;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
